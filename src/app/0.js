4d4e61870edaf58f463484e2ed3d9d74210a91a7
Ralf Th. Pietsch <ratopi@abwesend.de>
2014-11-23 22:59:18 +0100

    cleanup

--- src/app/0.js
+++ src/app/0.js
@@ -3,28 +3,6 @@ angular
 
 .module( "lernquiz", [ "ngRoute" ] )
 
-.value(
-	"config",
-	{
-		"root": window.location.href.replace( /\/_design\/.*/, "" ),
-		"idlen": 8
-	}
-)
-
-.run(
-	[ "$rootScope",
-	function ($rootScope)
-	{
-		$rootScope.$on(
-			"$locationChangeSuccess",
-			function (event, next, current)
-			{
-			}
-		);
-	}
-	]
-)
-
 .config(
 	[ "$routeProvider", "$httpProvider",
 	function ($routeProvider, $httpProvider)
